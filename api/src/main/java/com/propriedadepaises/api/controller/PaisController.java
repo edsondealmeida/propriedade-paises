5e8ba407a9aeb47b20a1e7d9da83c07001829c18
Edson de Almeida <edson.almeida23998@gmail.com>
2023-04-11 12:07:35 +0200

    Creating a method to save the pais introduced by the user

--- api/src/main/java/com/propriedadepaises/api/controller/PaisController.java
+++ api/src/main/java/com/propriedadepaises/api/controller/PaisController.java
@@ -2,12 +2,11 @@ package com.propriedadepaises.api.controller;
 
 import com.propriedadepaises.api.models.Pais;
 import com.propriedadepaises.api.repository.PaisRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +16,12 @@ public class PaisController {
 
     @Autowired
     private PaisRepository paisRepository;
+
+    @PostMapping("/paises")
+    @Transactional
+    public ResponseEntity creatingPais(@RequestBody Pais pais){
+        return ResponseEntity.status(HttpStatus.CREATED).body(paisRepository.save(pais));
+    }
 
     @GetMapping("/paises")
     public ResponseEntity<List<Pais>> findAll(){
